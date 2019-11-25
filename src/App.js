236b32e839f53956388a01ce7f894dc70cf3e083
Martin D <mdonchev@msn.com>
2019-11-25 14:28:55 +0200

    Adding HTML/App.js for testing the components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import BoilerplateComponent from "../src/smpladminui";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <hr />
+
+      <BoilerplateComponent />
+    </div>
+  );
+}
+
+export default App;
