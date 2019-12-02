55467e697c7ca838b3661043b4e5cdd1e35968ec
Martin D <mdonchev@msn.com>
2019-12-02 09:42:56 +0200

    Webpack changes

--- src/BoilerplateComponent.jsx
+++ src/BoilerplateComponent.jsx
@@ -1,49 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 const propTypes = {
-    label: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    styles: PropTypes.object
+	label: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+	styles: PropTypes.object
 };
 
 const defaultProps = {
-    styles: {
-        label: {
-            fontFamily: "Comic Sans MS",
-            color: "green"
-        },
-        input: {
-            background: "#ddd",
-            border: "1px solid red"
-        }
-    }
+	styles: {
+		label: {
+			fontFamily: "Comic Sans MS",
+			color: "green"
+		},
+		input: {
+			background: "#ddd",
+			border: "1px solid red"
+		}
+	}
 };
 
 class BoilerplateComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    handleChange(e) {
-        this.props.onChange(e.target.value);
-    }
+	handleChange(e) {
+		this.props.onChange(e.target.value);
+	}
 
-    render() {
-        const styles = this.props.styles || {};
+	render() {
+		const styles = this.props.styles || {};
 
-        return (
-            <div>
-                <label style={styles.label}>{this.props.label}</label>
-                <input
-                    type="text"
-                    style={styles.input}
-                    onChange={this.handleChange}
-                />
-            </div>
-        );
-    }
+		return (
+			<React.Fragment>
+				<label style={styles.label}>{this.props.label}</label>
+				<input
+					type="text"
+					style={styles.input}
+					onChange={this.handleChange}
+				/>
+			</React.Fragment>
+		);
+	}
 }
 
 BoilerplateComponent.propTypes = propTypes;
--- src/smpladminui.jsx
+++ src/smpladminui.jsx
@@ -1,21 +1,21 @@
-import BoilerplateComponent from "../src/BoilerplateComponent.jsx";
-import ButtonComponent from "../src/Button/ButtonComponent";
-import LayoutComponent from "../src/Layout/LayoutComponent";
-import PageComponent from "../src/Page/PageComponent";
-import DropmenuComponent from "../src/Dropmenu/DropmenuComponent";
-import CardComponent from "../src/Card/CardComponent";
-import ListComponent from "../src/List/ListComponent";
-import PopupComponent from "../src/Popup/PopupComponent";
-import IconComponent from "../src/Icon/IconComponent";
-import WrapperComponent from "../src/Wrapper/WrapperComponent";
+// import BoilerplateComponent from "./BoilerplateComponent.jsx";
+import ButtonComponent from "./Button/ButtonComponent";
+import CardComponent from "./Card/CardComponent";
+import DropmenuComponent from "./Dropmenu/DropmenuComponent";
+import IconComponent from "./Icon/IconComponent";
+import LayoutComponent from "./Layout/LayoutComponent";
+import ListComponent from "./List/ListComponent";
+import PageComponent from "./Page/PageComponent";
+import PopupComponent from "./Popup/PopupComponent";
+import WrapperComponent from "./Wrapper/WrapperComponent";
 
 // Theming
 import "./_theme_default.scss";
 
-export default BoilerplateComponent;
+export default ButtonComponent;
 
 export {
-	BoilerplateComponent,
+	// BoilerplateComponent,
 	ButtonComponent,
 	LayoutComponent,
 	PageComponent,
